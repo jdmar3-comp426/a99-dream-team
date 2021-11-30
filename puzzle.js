c07d214b186fde908c6dc72dec40751eecffe07d
Fangchen Chen <chenfc@live.unc.edu>
2021-11-29 20:10:46 -0500

    init

---
+++ puzzle.js
@@ -0,0 +1,8 @@
+/*
+    The puzzle is represented by an number array containing some permutation of numbers 0-23
+    number n represents the block in the original picture at coordinate (row=Math.floor(n/6), col=n%6) where the top-left is defined to be (0,0)
+    block n=23 is the bottom-right block that is changed to empty
+*/
+function makePuzzle(){
+    
+}
