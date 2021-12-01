2f2690b177ed0175636141e513de1c8a540aec32
Fangchen Chen <chenfc@live.unc.edu>
2021-12-01 15:27:52 -0500

    test

--- puzzle.js
+++ puzzle.js
@@ -4,7 +4,7 @@
     block n=23 is the bottom-right block that is changed to empty
 */
 
-function isSolvable(){
+function isSolvable() {
     /*
     Checks the number of "inversions". If this is odd then the puzzle configuration is not solvable.
 
@@ -23,29 +23,28 @@ function isSolvable(){
     */
 }
 
-function genPuzzle(){
+function genPuzzle() {
     // Generate a random permutation of 0-23
-    var puzzle=[];
-    var pool=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
-    while (puzzle.length<24){
-        var idx = Math.floor(Math.random()*puzzle.length);
+    var puzzle = [];
+    var pool = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+    while (puzzle.length < 24) {
+        var idx = Math.floor(Math.random() * puzzle.length);
         puzzle.push(pool.idx);
-        pool.splice(idx,1);
+        pool.splice(idx, 1);
     }
     return puzzle;
 }
 
-function makePuzzle(){
+function makePuzzle() {
     // check against isSolvable() return puzzle : generate another one and repeat
     var puzzle = genPuzzle();
-    while (isSolvable(puzzle)==False){
+    while (isSolvable(puzzle) == False) {
         puzzle = genPuzzle();
     }
     return puzzle;
-
 }
 
-function click(n){
+function click(n) {
     // n is the block that is clicked
     // check if n is next to the empty block  swap place check success return : do nothing return false
     // neighbor in 4*6 matrix means index is +-1 or +-6
