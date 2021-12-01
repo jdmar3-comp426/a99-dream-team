8eaf5b7ffab89fcaa01a69d8bc653256dc951544
Fangchen Chen <chenfc@live.unc.edu>
2021-12-01 15:16:22 -0500

    makepuzzle

--- puzzle.js
+++ puzzle.js
@@ -26,9 +26,17 @@ function isSolvable(){
 function makePuzzle(){
     // Generate a random permutation of 0-23
     // check against isSolvable() return puzzle : generate another one and repeat
+    var puzzle=[];
+    var pool=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
+    while (puzzle.length<24){
+        var idx = Math.floor(Math.random()*puzzle.length);
+        puzzle.push(pool.idx);
+        pool.splice(idx,1);
+    }
 }
 
 function click(n){
     // n is the block that is clicked
     // check if n is next to the empty block  swap place check success return : do nothing return false
+    // neighbor in 4*6 matrix means index is +-1 or +-6
 }
