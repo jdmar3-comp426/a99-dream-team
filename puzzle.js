d6ea402d53198d7dc084e645b5b09e80596aa923
Fangchen Chen <chenfc@live.unc.edu>
2021-12-01 19:17:16 -0500

    img display

--- puzzle.js
+++ puzzle.js
@@ -3,13 +3,9 @@
     number n represents the block in the original picture at coordinate (row=Math.floor(n/6), col=n%6) where the top-left is defined to be (0,0)
     block n=23 is the bottom-right block that is changed to empty
 */
-var db = require('./database.js');
+var puzzleArr;
+//var db = require('./database.js');
 const startbtn = document.querySelector('#start')
-startbtn.onclick = function(){
-    var puzzle = makePuzzle();
-    //TODO: STORE THIS PUZZLE IN DATABASE
-    //db.prepare("")
-}
 function isSolvable(puzzle) {
     /*
     Checks the number of "inversions". If this is odd then the puzzle configuration is not solvable.
@@ -28,17 +24,17 @@ function isSolvable(puzzle) {
     The empty tile is ignored.
     Theory credit to: https://datawookie.dev/blog/2019/04/sliding-puzzle-solvable/
     */
-   var numInv=0;
-   for (let i=0;i<24;i++){
-    for (let j=i;j<24;j++){
-        if(puzzle[i]!=0 && puzzle[j]!=0){
-            if(puzzle[i]>puzzle[j]){
-                numInv+=1;
+    var numInv = 0;
+    for (let i = 0; i < 24; i++) {
+        for (let j = i; j < 24; j++) {
+            if (puzzle[i] != 0 && puzzle[j] != 0) {
+                if (puzzle[i] > puzzle[j]) {
+                    numInv += 1;
+                }
             }
         }
     }
-   }
-   return numInv%2==0;
+    return numInv % 2 == 0;
 }
 
 function genPuzzle() {
@@ -54,30 +50,36 @@ function genPuzzle() {
 }
 
 function makePuzzle() {
-    console.log(1)
     // check against isSolvable() return puzzle : generate another one and repeat
     var puzzle = genPuzzle();
     while (isSolvable(puzzle) == false) {
         puzzle = genPuzzle();
     }
-    for (let i=0;i<23;i++){
-        document.getElementById(i).src  = "src/images/unc"+puzzle[i]+".jpg";
+    for (let i = 0; i < 23; i++) {
+        document.getElementById(i).src = "src/images/unc" + puzzle[i] + ".jpg";
     }
+    // TODO: store array in database and set step=0
+    // db.prepare().run()
+    puzzleArr = puzzle;
     return puzzle;
 }
 
-function click(n, arr) {  // - caleb
+function click(n) {  // - caleb
     // n is the block that is clicked
     // check if n is next to the empty block  swap place check success return : do nothing return false
     // neighbor in 4*6 matrix means index is +-1 or +-6 watch out for edge cases
 
     // if place is swapped and puzzle is not solved, return -1
-
-    if (arr[n] == 23) { 
+    // TODO: pull array from database
+    // var arr=db.prepare().get()
+    console.log(n)
+    arr=puzzleArr
+    console.log(arr)
+    if (arr[n] == 23) {
         return false
     } else if (n == 0) {
         if (arr[1] == 23 | arr[6] == 23) {
-            swap(n, arr) 
+            swap(n, arr)
         }
     } else if (n < 6 | n > 17) {
         if (arr[n + 1] == 23 | arr[n - 1] == 23) {
@@ -88,14 +90,23 @@ function click(n, arr) {  // - caleb
     } else { // returning false if no swap occurred
         return false
     }
-    return [checkSolved(arr),arr] // testing to see if the array is solved if a swap occurred
+    // TODO: store updated array back to database
+    // db.prepare().run()
+    if (checkSolved(arr)) {
+        // TODO: update database if step<beststep
+        // db.prepare().run()
+    }
+    return [checkSolved(arr), arr] // testing to see if the array is solved if a swap occurred
 }
 
 function swap(n, arr) { // this function swaps positions of n and the empty block within our puzzle array - caleb
+    //TODO: increment steps in database by 1
     var indexEmpty = arr.indexOf(23)
     var temp = arr[n]
     arr[n] = 23
     arr[indexEmpty] = temp
+    document.getElementById(n).src = "src/images/unc" + indexEmpty + ".jpg";
+    document.getElementById(indexEmpty).src = "src/images/unc" + n + ".jpg";
 }
 
 function checkSolved(arr) { // this function checks if our puzzle is solved (ie, sorted in ascending order) - caleb
